7411bc4e1fb6a62ddf960dc7bee175da84b190c8
Magali P Gutierrez <magali.g@bue.edu.ar>
2025-06-04 00:21:57 -0300

    microprocesador

--- src/main/java/ar/edu/unahur/obj2/command/ProgramBuilder.java
+++ src/main/java/ar/edu/unahur/obj2/command/ProgramBuilder.java
@@ -1,5 +1,18 @@
 package ar.edu.unahur.obj2.command;
 
-public class ProgramBuilder {
+import java.util.ArrayList;
+import java.util.List;
 
+import ar.edu.unahur.obj2.command.comandos.Operable;
+
+public class ProgramBuilder {
+     private List<Operable> operaciones = new ArrayList<>();
+     
+     public ProgramBuilder(List<Operable> opss) {
+        this.operaciones=opss;
+     }
+
+     public Programable build(){
+        return this;
+     }    
 }
